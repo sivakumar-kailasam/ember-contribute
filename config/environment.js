e36816f9e1e85e3ed7f3555db1c741fc85a29812
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2016-04-09 19:30:22 +0530

    CSP headers

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,14 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.contentSecurityPolicy = {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self' http://fonts.gstatic.com",
+      'connect-src': "'self'",
+      'img-src': "'self' data:",
+      'media-src': "'self'"
+    };
   }
 
   return ENV;
